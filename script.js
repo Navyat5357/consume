5ea5863fddd997819265926c98442b280eff3c94
Navyat5357 <145207187+Navyat5357@users.noreply.github.com>
2023-12-21 22:53:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+
+
+// Function to toggle the menu for small screens
+function toggleMenu() {
+    var menu = document.getElementById("menuButton");
+    menu.classList.toggle("show-menu");
+}
+
+// You can add more JavaScript functions as needed
